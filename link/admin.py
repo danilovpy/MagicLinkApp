7bfee91043d760cfa6bb6035f54d3945ccb72392
Sergey <danylov.sergey1@gmail.com>
2021-02-14 00:33:44 +0200

    fixing hashing in admin panel and adding whitenoise

--- link/admin.py
+++ link/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import User
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 
 
-admin.site.register(User)
+class MyUserChangeForm(UserChangeForm):
+
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = "__all__"
+
+
+class MyUserAdmin(UserAdmin):
+
+    form = MyUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets
+
+
+admin.site.register(User, MyUserAdmin)
